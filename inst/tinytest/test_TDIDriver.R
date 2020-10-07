73230f073650d9a7596cad39fb41dd774ceedcc2
nordicgit70 <nordicgit70@users.noreply.github.com>
2020-10-07 14:55:44 +0200

    Improved TDI driver test coverage.

--- inst/tinytest/test_TDIDriver.R
+++ inst/tinytest/test_TDIDriver.R
@@ -3,3 +3,4 @@ source("helper-Mock.R")
 # Test TDIDriver constructor.
 expect_true(inherits(driver("mock"), "TDIDriver"))
 expect_true(inherits(driver("mock"), "mock"))
+expect_error(driver("blablabla"))
