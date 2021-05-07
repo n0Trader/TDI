79b6ae67f2a3c4dffbd6143646975ec97be61996
nordicgit70 <nordicgit70@users.noreply.github.com>
2021-05-07 02:57:59 +0200

    Final improvement TDIResult fields and validate methods.

--- R/TDIResult.R
+++ R/TDIResult.R
@@ -17,34 +17,66 @@ TDIResult <- R6::R6Class("TDIResult", inherit = baseTDI,
   public = list(
     #' @description
     #' Return the properties of the object as a list.
+    #' @param prop Optional property in recursive call.
     #' @return List of object properties.
-    fields = function() {
-      f <- sapply(self, function(x) {
-        # Skip function(s) and environment(s).
-        if (is.function(x)) { return()
-        } else if (is.environment(x)) {
-          if (is.baseTDI(x)) return(list(x$fields()))
-          else return()
-        } else { return(x) }
-      }, USE.NAMES = TRUE)
-      return(as.list(do.call(c, f)))
+    fields = function(prop) {
+      if (missing(prop)) {
+        # Recursive through properties and skip functions.
+        fields <- list()
+        for (name in names(self)) {
+          prop <- get(name, envir = self)
+          if (!is.function(prop) && !is.null(prop)) {
+            fields[[name]] <- self$fields(prop)
+          }
+        }
+        return(rev(fields))
+        
+      } else if (is.environment(prop)) {
+        # Recursive call TDI base objects and skip other environments.
+        if (is.baseTDI(prop)) {
+          return(prop$fields())
+        } else return()
+        
+      } else if (is.list(prop)) {
+        res <- list()
+        for (i in 1:length(prop)) {
+          res[[i]] <- self$fields(prop[[i]])
+        }
+        return(res)
+        
+      } else if (is.na(prop)) { return(NULL)
+      } else return(prop)
     },
     
     #' @description
     #' Validate the properties of the object.
     #' It is assumed that all NA properties are optional.
+    #' @param prop Optional property in recursive call.
     #' @return Vector with validation result.
-    validate = function() {
-      f <- sapply(self, function(x) {
-        # Skip function(s) and environment(s).
-        if (is.function(x)) { return()
-        } else if (is.environment(x)) { 
-          if (is.baseTDI(x)) return(x$validate())
-          else return()
-        } else if (is.null(x)) { return(FALSE)
-        } else return(TRUE)
-      }, USE.NAMES = TRUE)
-      return(do.call(c, f))
+    validate = function(prop) {
+      if (missing(prop)) {
+        f <- sapply(self, function(x) {
+          if (is.function(x)) { return()
+          } else return(self$validate(x))
+        }, USE.NAMES = TRUE, simplify = TRUE)
+        return(unlist(do.call(c, f)))
+        
+      } else if (is.environment(prop)) {
+        # Recursive call TDI base objects and skip other environments.
+        if (is.baseTDI(prop)) {
+          return(prop$validate())
+        } else return()
+        
+      } else if (is.list(prop)) {
+        res <- list()
+        for (i in 1:length(prop)) {
+          res[[i]] <- self$validate(prop[[i]])
+        }
+        return(res)
+
+      } else if (is.null(prop)) { return(FALSE)
+      } else if (is.na(prop)) { return(NULL)
+      } else return(TRUE)
     },
 
     #' @description
