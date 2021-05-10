5257b900f245f410485b45a401d61ffd35b7f82b
nordicgit70 <nordicgit70@users.noreply.github.com>
2021-05-10 15:15:48 +0200

    Renamed OHLC sources.

--- startup.R
+++ startup.R
@@ -1,10 +1,19 @@
 #' Start plumbing.
-p <- plumber::plumb("R/plumber.R")
+p <- plumber::plumb("R/plumber.R") %>%
+  plumber::pr_cookie(
+    plumber::random_cookie_key(),
+    name = "counter"
+  ) %>% plumber::pr_hook("exit", function(req) {
+    message("Exit plumber!")
+  }) %>% plumber::pr_get("/session", function(req) {
+    if (!is.null(req$session$counter)){
+      count <- as.numeric(req$session$counter)
+    } else  count <- 0
+    req$session$counter <- count + 1
+    return(paste0("This is visit #", count))
+  })
 
-p$registerHook("exit", function() {
-  message("Exit plumber!")
-})
-
+# Start the plumber API.
 p$run(
   port = as.integer(Sys.getenv("TDI_port", unset = "8585")),
   host = Sys.getenv("TDI_host", unset = "0.0.0.0"),
